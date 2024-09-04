54acb9ae8ef1798eb317a000219808ceb6846e8f
Sebastian Gaus <Sebastian.Gaus@enders-training.de>
2024-09-04 09:25:53 +0200

    added filter in template and created a categorizing function in app.py

--- app.py
+++ app.py
@@ -3,17 +3,27 @@ from flask import Flask, render_template, request
 # Initialize the Flask application
 app = Flask(__name__)
 
+def categorize_bmi(bmi):
+    if bmi < 18.5:
+        return "you are underweight - eat more!"
+    elif bmi < 25:
+        return "you have normal weight - keep on going!"
+    else:
+        return "you are overweight - eat less and move more!"
+
 # Define a route for the home page
 @app.route('/', methods=['GET', 'POST'])
 def index():
     bmi = None
+    cat = None
     if request.method == 'POST':
-        height = request.form['height-m']
-        weight = request.form['weight-kg']
-        bmi = round(weight / height**2, 2)
+        height = float(request.form['height-m'])
+        weight = float(request.form['weight-kg'])
+        bmi = round(weight / (height/100)**2, 2)
+        cat = categorize_bmi(bmi)
     
     # Render the template
-    return render_template('index.html')
+    return render_template('index.html', bmi=bmi, cat=cat)
 
 if __name__ == '__main__':
     app.run(debug=True)
