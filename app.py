598d38574503f1c0499f6a5107138b8cf7cd4bd4
Sebastian Gaus <Sebastian.Gaus@enders-training.de>
2024-09-04 08:49:29 +0200

    First initalization of server

--- app.py
+++ app.py
@@ -2,3 +2,13 @@ from flask import Flask, render_template
 
 # Initialize the Flask application
 app = Flask(__name__)
+
+# Define a route for the home page
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    
+    # Render the template
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
